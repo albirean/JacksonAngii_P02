6924da330b99bcc98a7c44dd354d386bd9453889
angii <sdj170130@utdallas.edu>
2020-10-23 16:53:36 -0500

    StateMachine Initial

---
+++ Assets/Scripts/Deck/CardDeck.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class CardDeck <T>
+{
+    public event Action OnEmptyDeck = delegate { }; // when the card count hits 0
+
+    private List<T> _cards = new List<T>();
+    public List<T> Cards
+    {
+        get { return _cards; }
+        set { _cards = value; }
+    }
+
+    public int Count
+    {
+        get { return Cards.Count; }
+    }
+
+    public CardDeck(List<T> cards)
+    {
+        this._cards = cards;
+    }
+
+    public int LastIndex
+    {
+        get
+        {
+            if (_cards.Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return _cards.Count - 1;
+            }
+        }
+    }
+
+    private int GetIndexFromPosition(DeckPosition position)
+    {
+        int newPositionIndex = 0;
+        // if deck is empty, index should always be 0
+        if (_cards.Count == 0)
+        {
+            newPositionIndex = 0;
+        }
+        //get end infex if it's on 'from the top'
+        if (position == DeckPosition.Top)
+        {
+            newPositionIndex = LastIndex;
+        }
+
+        //randomize if drawing from the middle
+        else if (position == DeckPosition.Middle)
+        {
+            newPositionIndex = UnityEngine.Random.Range(0, LastIndex);
+        }
+
+        // get 0 index if it's from the bottom
+        else if (position == DeckPosition.Bottom)
+        {
+            newPositionIndex = 0;
+        }
+
+        return newPositionIndex;
+    }
+
+    public void Add(List<T> cards, bool onTop = true)
+    {
+        if (onTop)
+        {
+            _cards.InsertRange(0, cards);
+        }
+        else
+        {
+            _cards.AddRange(cards);
+        }
+    }
+
+    public void Add(T card, DeckPosition position = DeckPosition.Top)
+    {
+        //bodyguard
+        if (card == null) { return; }
+        int targetIndex = GetIndexFromPosition(position);
+        // to add it to the 'Top' add it at the end.
+        // by default Insert() moves index upward.
+        if (targetIndex == LastIndex)
+        {
+            _cards.Add(card);
+        }
+        else
+        {
+            _cards.Insert(targetIndex, card);
+        }
+    }
+
+    public void Remove(int cardIndex)
+    {
+        if(cardIndex < 0 || cardIndex > Cards.Count - 1) { return; } // index doesn't fall within range
+        //find out which card to remove
+        Cards.RemoveAt(cardIndex);
+    }
+
+    public List<T> Draw(int numberToDraw, bool onTop = true)
+    {
+        if(numberToDraw <= 0) { return new List<T>(); } // drawing 0 cards doesn't work
+
+        // if we're asked to draw too many, only draw what we can
+        if(numberToDraw > _cards.Count)
+        {
+            numberToDraw = _cards.Count;
+        }
+        // draw the cards we're able to
+        List<T> drawnCards = new List<T>();
+        for (int i = 0; i < numberToDraw; i++)
+        {
+            drawnCards.Add(_cards[0]);
+            _cards.RemoveAt(0);
+        }
+        //check if we've ran out of cards
+        if(Cards.Count == 0)
+        {
+            OnEmptyDeck.Invoke();
+        }
+        return drawnCards;
+    }
+
+    public void Shuffle()
+    {
+        for (int i = _cards.Count -1; i>0; --i)
+        {
+            int j = UnityEngine.Random.Range(0, i + 1);
+            T card = _cards[j];
+            _cards[j] = _cards[i];
+            _cards[i] = card;
+        }
+    }
+}
---
+++ Assets/Scripts/Deck/CardDeckSystem.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class CardDeckSystem<T>
+{
+    public CardDeck<T> DrawPile { get; private set; }
+    public CardDeck<T> Hand { get; private set; }
+    public CardDeck<T> DiscardPile { get; private set; }
+
+    public event Action<int> OnDrawPileOverdraw = delegate { };
+    public event Action<int> OnDiscardPileOverdraw = delegate { };
+
+    public CardDeckSystem(List<T> cards)
+    {
+        DrawPile = new CardDeck<T>(cards);
+        //hand and discard need to start off with empty decks
+        List<T> emptyDeck = new List<T>();
+        Hand = new CardDeck<T>(emptyDeck);
+        DiscardPile = new CardDeck<T>(emptyDeck);
+    }
+
+    public void Draw(int numberToDraw = 1)
+    {
+        // drawing 0 cards doesn't make sense
+        if(numberToDraw <= 0) { return; }
+        //if we're able to draw multiple, do it
+        if(DrawPile.Count >= numberToDraw)
+        {
+            List<T> drawnCards = DrawPile.Draw(numberToDraw);
+            Hand.Add(drawnCards);
+        }
+
+        // not enough cards! Trigger overdraw event.
+        else
+        {
+            //track the number of remaining cards we will have, because we are about to alter data
+            int undrawnCards = numberToDraw - DrawPile.Count;
+            numberToDraw = DrawPile.Count;
+            //draw the cards
+            List<T> drawnCards = DrawPile.Draw(numberToDraw);
+            //send out notif on remaining cards
+            OnDrawPileOverdraw.Invoke(undrawnCards);
+        }
+    }
+
+    public void DrawFromDiscard(int numberToDraw = 1)
+    {
+        if (numberToDraw == 0) { return; } // drawing 0 cards doesn't make sense
+        // since we're drawing from discard, typically wanna draw from bottom
+        List<T> drawnCards = DiscardPile.Draw(numberToDraw, false);
+        Hand.Add(drawnCards, false);
+        //account for running out of cards to draw
+        if (numberToDraw > drawnCards.Count)
+        {
+            int undrawnCards = numberToDraw - drawnCards.Count;
+            OnDiscardPileOverdraw.Invoke(undrawnCards);
+        }
+    }
+
+    public void Discard(int cardIndex)
+    {
+        if(cardIndex < 0 || cardIndex > Hand.Cards.Count - 1) { return; } // index doesn't fall within range
+        // find out which card to remove
+        T cardToDiscard = Hand.Cards[cardIndex];
+        //pass it through
+        Hand.Remove(cardIndex);
+        DiscardPile.Add(cardToDiscard); // add to bottom.
+    }
+
+    public void Discard(List<int> cardIndexes)
+    {
+        if(cardIndexes.Count > Hand.Cards.Count) { return; } // can't discard more than we have!
+
+        // create the list of cards to Remove
+        List<T> cardsToDiscard = new List<T>();
+        //grab refs to cards we want to discard
+        foreach(int cardIndex in cardIndexes)
+        {
+            // validate the index
+            if(cardIndex < 0 || cardIndex > Hand.Cards.Count)
+            {
+                continue;
+            }
+            cardsToDiscard.Add(Hand.Cards[cardIndex]);
+        }
+        //discard them
+        foreach(T card in cardsToDiscard)
+        {
+            Hand.Cards.Remove(card);
+            //discard piles are typically face up, meaning we want to add to the bottom
+            DiscardPile.Add(card);
+        }
+        //cleanup
+        cardsToDiscard.Clear();
+    }
+
+    public void DiscardRandom()
+    {
+        //TODO
+    }
+
+    public void DiscardHand()
+    {
+        for (int i = 0; i < Hand.Count; i++)
+        {
+            Discard(i);
+        }
+    }
+
+    public void RemoveFromHand(List<int> cardIndexes)
+    {
+        //TODO
+    }
+
+    public void ShuffleDiscardIntoDrawPile()
+    {
+        foreach (T cardToShuffle in DiscardPile.Cards)
+        {
+            DrawPile.Add(cardToShuffle);
+        }
+        DiscardPile.Cards.Clear();
+        DiscardPile.Shuffle();
+    }
+}
